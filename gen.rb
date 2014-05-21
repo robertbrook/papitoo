c6703eac49b0401b615267045744d83d4b4bf6c1
Robert Brook <mail@robertbrook.com>
2014-05-21 14:23:30 +0100

    scratching about

--- gen.rb
+++ gen.rb
@@ -1,21 +1,38 @@
 require 'nokogiri'
+require 'time'
+
+# http://nokogiri.org/Nokogiri/XML/SAX/Document.html
+# grep -Hnrc --colour "hs_TimeCode time=\"2000" *
 
 class HansardDoc < Nokogiri::XML::SAX::Document
+
 def start_element name, attrs = []
-#   puts "starting: #{name}"
-  @is_hs_para = name.eql?("hs_para")
+  if name.eql?("hs_TimeCode")
+    target = attrs[0][1]
+    puts Time.parse target
+    if Time.parse(target).to_i == 946684800
+      puts "^^^^^^^^dodgy!"
+    end
+  end
 end
 
 def characters string
-    string.strip!
-    if @is_hs_para and !string.empty?
-      puts "#{string}"
-    end
+
+    # string.strip!
+#     if @is_hs_TimeCode and !string.empty?
+#       puts string
+#     end
 end
 
 def end_element name
-#   puts "ending: #{name}"
+    
 end
+
+def processing_instruction(name, content)
+#   puts name
+#   puts content
+end
+
 end
 
 parser = Nokogiri::XML::SAX::Parser.new(HansardDoc.new)
